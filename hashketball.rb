f9ce8177541517c528dc05e6899c84dff908a5d5
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-26 19:11:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,7 +192,7 @@ def player_stats(players_name)
     if key == :players
       value.each do |player|
          if players_name == player[:player_name]
-           player.delete [:player_name]
+           player.delete(:player_name)
            return players_name[:player_name]
          end
         end
