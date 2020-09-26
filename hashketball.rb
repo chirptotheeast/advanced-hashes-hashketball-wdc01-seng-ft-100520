8f8687efce46ad2900879999d877ad2a3d344518
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-26 19:31:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,6 +207,7 @@ def big_shoe_rebounds
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
       if player[:shoe] > largest_shoe_size
+        largest_shoe_size = player[:shoe]
     binding.pry
     end
   end
